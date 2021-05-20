30e3b4e1cba6d050efdbcb59948e1bfffb21e102
Andy Guo <>
2021-05-20 16:52:07 +0000

    Test run command

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -2,4 +2,13 @@ package recursionAssignment;
 
 public class Recursion{
 
+  /*
+  * A test method to check if our test class functions
+  * 
+  * @return true
+  * @author: Andy Guo
+  */
+  public static boolean testMethod() {
+    return true;
+  }
 }
--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -1,34 +1,44 @@
 package recursionAssignment;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+/*
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Before;
 import org.junit.jupiter.api.After;
+*/
 
 import recursionAssignment.Recursion;
 
 public class RecursionTest{
-    
+
+    /*
     @Before
     public void beforeTest(){
       // some steps to perform before tests are run
     }
+    */
     
     /**
      * An initial test
+     * Testing run command
      */
     @Test
-    public void Test1(){
+    public void TestInitial(){
         // make assertion statement(s)
         // assertEquals(EXPECTEDVALUE, TESTVALUE);
         // assertTrue(TESTBOOLEAN);
+        assertEquals(true, Recursion.testMethod());
     }
     
     // add more tests
     
+    /*
     @After
     public void afterTest(){
       // some steps to perform after tests are run
     }
+    */
     
     
 }
